99e222a66c4546fce86f02e8dbe92d7f649ce8ca
ADMINSLOT99 <frdiboy.01@gmail.com>
2025-04-07 00:17:10 +0700

    Menambahkan fitur update dan delete

--- index.js
+++ index.js
@@ -16,6 +16,38 @@ app.post("/users", (req, ress) => {
   });
 });
 
+//update data
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { nama, email } = req.body;
+
+  const sql = "UPDATE users SET nama = ?, email = ? WHERE id = ?";
+  db.query(sql, [nama, email, id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("data gagal di update");
+    }
+    if (result.affectedRows === 0) {
+      return res.status("data tidak ditemukan");
+    }
+    res.send("data berhasil di update");
+  });
+});
+
+//hapus data
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const sql = "DELETE FROM users WHERE id = ?";
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("data gagal di hapus", err);
+    }
+    res.send("data berhasil di hapus");
+  });
+});
+
 app.listen(3000, () => {
   console.log("server berhasil berjalan di http://localhost:3000");
 });
